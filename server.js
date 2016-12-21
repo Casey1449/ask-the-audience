f1b86597402976295220bc92fe2aa8c6a0399549
Casey Metz <caseymetz@gmail.com>
2016-12-21 10:06:27 -0700

    follow-along portion working

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const express = require('express');
 
 const app = express();
 
+let votes = {};
+
+function countVotes(votes) {
+  var voteCount = {
+      A: 0,
+      B: 0,
+      C: 0,
+      D: 0
+  };
+  for (var vote in votes) {
+    voteCount[votes[vote]]++
+  }
+  return voteCount;
+}
+
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res){
@@ -15,5 +31,30 @@ const server = http.createServer(app)
                  .listen(port, function () {
                     console.log('Listening on port ' + port + '.');
                   });
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.emit('statusMessage', 'You have connected.');
+
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      socket.emit('voteCount', countVotes(votes));
+    }
+  });
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    socket.emit('voteCount', countVotes(votes));
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
+  });
+});
 
 module.exports = server;
